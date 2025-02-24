0e3322c7356a28b9f1eb6d8b3dd6286e1b316ca4
ankitjhagithub21 <ajha45075@gmail.com>
2025-02-25 04:02:52 +0530

    minor changes

--- backend/models/user.js
+++ backend/models/user.js
@@ -29,7 +29,7 @@ const userSchema = new Schema(
       type:Boolean,
       default:false
     },
-    verificationToken: { type: String,default:null },
+    verificationToken: { type: String},
     
   },
   
